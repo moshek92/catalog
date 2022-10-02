edbab82728de877928ee3dfa7963bfacad4e82e1
=moshe <=moshikkassin@gmail.com>
2022-10-02 20:27:25 +0300

    index php

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title> the sport catalog</title>
+</head>
+
+<body>
+    <h1>
+        the sport catalog
+    </h1>
+
+    <?php
+
+    $customer_name = "big store";
+    echo "<p>", $customer_name,  "</p>";
+
+
+
+    
+    ?>
+    <p>
+        <?php
+        echo $customer_name; 
+        
+        ?>
+    </p>
+    <ul>
+        <li>
+
+        </li>
+        <li>
+
+        </li>
+        <li>
+
+        </li>
+    </ul>
+
+</body>
+
+</html>
